97783bd6b70f8e6997b5f898aeb2260cf4534abe
Miguel Tomé <53304369+miguelptome@users.noreply.github.com>
2019-07-25 15:35:46 +0100

    Create P10773 - Back to Intermediate Math

---
+++ P10773.java
@@ -0,0 +1,27 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class P10773 {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int n = scn.nextInt();
+		for (int i=1; i<=n; i++) {
+			double d = scn.nextDouble();
+			double v = scn.nextDouble();
+			double u = scn.nextDouble();
+			if (v == 0 || u == 0 || u <= v) {
+				out.println("Case " + i + ": can't determine");
+				continue;
+			}
+			double fastest = d/u;
+			double shortest = d / Math.sqrt(u * u - v * v);
+			double r = Math.abs(fastest - shortest);
+			out.print("Case " + i + ": ");
+			out.printf("%.3f\n", r);
+		}
+		out.flush();
+		out.close();
+		scn.close();
+	}
+}

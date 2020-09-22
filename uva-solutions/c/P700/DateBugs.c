a78cc838330e9aa24e86beca5d69b577f855154e
miguelptome <miguelptome@gmail.com>
2020-09-22 14:40:36 +0100

    Base solution to the problem 700 from UVA.

---
+++ uva-solutions/c/P700/DateBugs.c
@@ -0,0 +1,16 @@
+/** 
+ * Author: Miguel Tomé
+ * Purpose: correct the year from corrupted dates
+ * Date: 21-09-2020
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    /* this problem have nothing to do with bits and bytes and 
+        with shit operators 
+     */
+
+    return 0;
+}

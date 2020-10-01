b72e10140ee89964860ad458cd60ee031375b32f
miguelptome <miguelptome@gmail.com>
2020-10-01 18:47:52 +0100

    Some libraries to math with MIN and MAX constant functions.

---
+++ uva-solutions/c/Library/Math.c
@@ -0,0 +1,11 @@
+/**
+ * Author: Miguel Tomé
+ * Purpose: This file have the purpose of having some common and useful functions
+ *  that are not available on standard lybrary of c.
+ * Date: 01-10-2020
+ */
+
+#include <stdio.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
